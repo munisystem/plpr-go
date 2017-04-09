5163c733dfb3e6c58b3f1d0cd2310a5c67bd2c61
Yuichi Saito <munisystem@gmail.com>
2017-04-09 21:06:52 +0900

    Add sample code

---
+++ example/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/munisystem/plpr-go"
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+	format := "[%m] %h:%d "
+	b, err := ioutil.ReadFile("postgresql.log")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	body := string(b)
+	logs := plpr.Parse(body, format)
+	for _, v := range logs {
+		fmt.Println("-----")
+		fmt.Println("StartTime: ", v.StartTime)
+		fmt.Println("EndTime: ", v.EndTime)
+		fmt.Println("Host: ", v.Host)
+		fmt.Println("DBName: ", v.DBName)
+		fmt.Println("Duration: ", v.Duration)
+		fmt.Println("Query : ", v.Query)
+	}
+}
